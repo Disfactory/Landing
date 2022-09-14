eed62533374e74e7d66aac2bdf1a09a7c93ef010
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2022-09-14 15:27:34 +0800

    CSS adjust

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -21,11 +21,11 @@ return (
     {/* 上方fix navbar */}
     <div className="navbar">
 
-      <div className='container'>
+      <div className='nav-container'>
 
           {/* TODO: 為什麼img沒辦法下載？是被什麼擋下來了嗎？ */}
           {/* 左方LOGO */}
-          <div className='logo'>
+          <div className='nav-logo'>
               <img src={logo} alt="" />
               <h1>農地違章工廠</h1>
           </div>
@@ -62,12 +62,9 @@ return (
           </div>
 
           {/* hambergur */}
-          {/* <div>
+          <div className='nav-hamburger'>
             <img alt="" src={hamburger}/>
-          </div> */}
-
-
-          
+          </div>
 
       </div>
 
@@ -76,16 +73,16 @@ return (
       
     </div>
 
-    <div className='forehead-container'>
+    <div className='forehead'>
     
     {/* BG */}
-    <img src={forehead_bg1} alt=""/>
-    <img src={forehead_bg2} alt=""/>
+    <img src={forehead_bg1} alt="" className='forehead_bg_left'/>
+    <img src={forehead_bg2} alt="" className='forehead_bg_right'/> 
 
         {/* 內文模式 */}
-        <div>
-          <h2>農地違章工廠回報系統</h2> 
-          <p>用科技工具改變政府</p>
+        <div className='forehead-content'>
+          <h2 className='main-title'>農地違章工廠回報系統</h2> 
+          <p className='subtitle'>用科技工具改變政府</p>
 
           <div className='forehead-btn-wrap'>
                 <a href="https://bit.ly/3kESSsQ">拍照回報</a>
@@ -97,27 +94,27 @@ return (
 
     
     {/* 你知道嗎？ */}
-    <div className='do-u-know'>
+    <div className='do-you-know'>
 
-        
-          <h2>你知道嗎？</h2>
-          <p>台灣沒被污染的農地很稀少， <br/>
+        <div className='do-you-know-title'>
+          <h2 className='main-title'>你知道嗎？</h2>
+          <p className='subtitle'>台灣沒被污染的農地很稀少， <br/>
           但沒日本製造的壓縮機少。</p>
+        </div>
 
-          <div className='do-u-know-content'>
+          <div className='do-you-know-content'>
 
-            <div>
-              <span>
-              農地被污染，你也不好過。臺灣過去三十年間工、商業高速發展、工業聚落及都市發展逐漸擴張至農業區域，導致國土使用規劃趨於紊亂，公共排水及下水道設施建設不及，環境污染因而伴隨而來。<br/> 
-              
-              綜觀而言，農地污染多集中於工業聚落下游與工農使用混雜區，反映出工農生產區位不相容的環境污染根本問題。工廠產生的廢棄物及廢水，或石化業及廢五金燃燒產生的排煙及落塵等問題，均可能造成土壤不同程度的污染。 <br/>
-              其中含重金屬的工廠廢水隨著灌溉水流進農地，以及受限於水量分配被迫引灌水質不良的迴歸水等兩因素，為農地遭重金屬污染的主因。
-              </span>
-            </div>
+              <div className='description'>
+              80年代的鎘米事件，讓台灣驚覺農地純淨不在、食安深陷危機；30年後的今天，全台每年仍有 3000~6000 間違法農地廠房持續新增在各個縣市。電鍍、染製廢水......流進田間灌溉渠道，違法農地工廠破壞台灣農地，唯有透過為農地止血與整體空間佈局才能守護糧食安全。
+              </div>
 
-            <div>
-              <img src={pollutone} alt=""/>
-            </div>
+              <div className='description'>
+              地球公民長達 6 年來從各種角度與層級切入，嘗試杜絕農地違章工廠帶來的陋習與衝擊。台灣地狹人稠，這就是我們唯一的家，如果發展欠缺規劃、不受管制，將為國土在生態和生計上都帶來浩劫。
+              </div>
+
+              <div className='description'>
+              我們是第⼀個發起行動整頓農地違章⼯廠的環境組織，終於讓政府從半世紀以來的不作為變成有作為，在扼⽌新增建農地⼯廠獲得相當進展。
+              </div>
 
           </div>
 
@@ -126,57 +123,59 @@ return (
     {/* 於是，我們製作了這些工具！ */}
     <div className='disfactory-tools'>
 
-          <h2>於是，我們製作了這些工具！</h2>
+          <h2 className='main-title'>於是，我們製作了這些工具！</h2>
 
           <div className='tools-container'>
+
                 <div className='disfactory-callout'>
-                  <h2 className='tool-h2-1'>違章工廠通報系統</h2>
+                  <h2 className='tools-subtitle'>違章工廠通報系統</h2>
                   <img src={disfactory_callout} alt=""/>
-                  <p>傳統的檢舉需要留下個人資料， <br/>
+                  <p className='tools-description'>傳統的檢舉需要留下個人資料， <br/>
                   且程序往往複雜。<br/>
                   違章工廠檢舉系統協助你快速、匿名檢舉，還不用擔心被找碴！</p>
                   
                   <div className='callout-tips'>
-                    <div className='num'>1</div>
+                    <div className='tools-num'>1</div>
                     <p>拍照</p>
                   </div>
+                  
                   <div className='callout-tips'>
-                    <div className='num'>2</div>
+                    <div className='tools-num'>2</div>
                     <p>標記地點+上傳</p>
                   </div>
+                  
                   <div className='callout-tips'>
-                    <div className='num'>3</div>
+                    <div className='tools-num'>3</div>
                     <p>地球公民基金會幫你匿名檢舉</p>
                   </div>
 
-                  <a>新增可疑工廠</a>
+                  <a className='tools-btn' href="https://bit.ly/3kESSsQ">新增可疑工廠</a>
 
 
                 </div>
             
                 <div className='findout-disfactory'>
-                  <h2 className='tool-h2-2'>大家來找廠</h2>
+                  <h2 className='tools-subtitle'>大家來找廠</h2>
                   <img src={playgame} alt=""/>
-                  <p>住在都市也想幫忙？ <br/>
-                  一起用遊戲改變世界！進入「大家來找廠」<br/>
+                  <p className='tools-description'>住在都市也想幫忙？ <br/>
+                  一起用遊戲改變世界！<br/>進入「大家來找廠」
                   一起用空拍圖辨識</p>
 
                   <div className='findout-tips'>
-                    <div className='num'>1</div>
+                    <div className='tools-num'>1</div>
                     <p>比對判斷空拍圖</p>
                   </div>
                   <div className='findout-tips'>
-                    <div className='num'>2</div>
+                    <div className='tools-num'>2</div>
                     <p>送出！</p>
                   </div>
                   <div className='findout-tips'>
-                    <div className='num'>3</div>
+                    <div className='tools-num'>3</div>
                     <p>地球公民基金會批次檢舉</p>
                   </div>
+            
+                  <a className='tools-btn' href="https://bit.ly/3vFXXaj">馬上鍵盤參與</a>
 
-              
-                
-                  <a>馬上鍵盤參與</a>
                 </div>
             
 
@@ -189,36 +188,34 @@ return (
 
     {/* 你的資料如何被運用？ */}
     <div className='how-to-use'>
-      <h2>你的資料如何被運用？</h2>
-      <p>您在系統上的每個微小行動，  <br/>
+      <h2 className='main-title'>你的資料如何被運用？</h2>
+      <p className='subtitle'>您在系統上的每個微小行動，  <br/>
           都將成為維護農地環境的有利證據！</p>
     </div>
 
     {/* 小小行動，大大改變 */}
     <div className='action-change'>
-      <h2>小小行動，大大改變</h2>
-      <p>這幾年我們不斷使用工具，  <br/>
+      <h2 className='main-title'>小小行動，大大改變</h2>
+      <p className='subtitle'>這幾年我們不斷使用工具，  <br/>
           讓違章工廠無所遁形！</p>
 
       {/* 行動3 steps */}
       <div className='action-wrap'>
           <div>
             <img src={action1} alt=""></img>
-            <p>513</p>
-            <p>累積回報人次</p>
+            <p className='action-num'>513</p>
+            <p className='action-notion'>累積回報人次</p>
           </div>
           <div>
             <img src={action2} alt=""></img>
-            <p>9999</p>
-            <p>累積申訴公文</p>
+            <p className='action-num'>9999</p>
+            <p  className='action-notion'>累積申訴公文</p>
           </div>
           <div>
             <img src={action3} alt=""></img>
-            <p>888</p>
-            <p>間被裁罰工廠</p>
+            <p className='action-num'>888</p>
+            <p className='action-notion'>間被裁罰工廠</p>
           </div>
-
-
 
       </div>
 
@@ -229,8 +226,8 @@ return (
 
     {/* 我們是誰？ */}
     <div className='who-we-are'>
-      <h2>我們是誰？</h2>
-      <p>既然你誠心誠意的發問了  <br/>
+      <h2 className='main-title'>我們是誰？</h2>
+      <p className='subtitle'>既然你誠心誠意的發問了  <br/>
           我們就大發慈悲的告訴你</p>
     </div>
 
