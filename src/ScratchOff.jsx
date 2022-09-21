ccf715f5175ee159245b1afc28ac6f377207981f
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2022-09-21 17:26:26 +0800

    [feat] create SractchOff component & scss

--- src/ScratchOff.jsx
+++ src/ScratchOff.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ScratchCard from "react-scratchcard";
 
-import cardImage from "./imgs/01.png";
-import cardImage2 from "./imgs/02.png";
-import './scratch.css';
+import cardImage from "./imgs/scratch_front.svg";
+import cardImage2 from "./imgs/scratch_back.svg";
+import './styles/scratch.css';
 
 const settings = {
   width: 400, 
   height: 300,
   image: cardImage,
-  finishPercent: 100, //這邊是完成比率
+  finishPercent: 100, //complete percent
   // onComplete: () => console.log("The card is now clear!")
 };
 const ScratchCardComponent = () => (
   <ScratchCard {...settings}>
-    <img src={cardImage2 } width="400" height="300"></img>
+    <img src={cardImage2 } width="400" height="300" alt=""></img>
   </ScratchCard>
 );
 
