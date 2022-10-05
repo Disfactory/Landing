b61fe3c497c71ca40e0051959969df5e5ee868c9
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2022-10-05 16:47:09 +0800

    [ feat ] : QA點擊題目後表頭icon會有CSS effect
    調整項目：
    1. 新增 function toggleEffect( ) ，點擊題目表頭icon時會觸發CSS animation
    2. 題目表頭icon從SVG素材改為CSS（toggle-opened/toggle-closed）

--- src/components/QA.jsx
+++ src/components/QA.jsx
@@ -5,14 +5,32 @@ import qa_plus from '../imgs/qa_plus.svg';
 
 function QA () {
 
+
   function answerShow(e) {
+
+    e.stopPropagation();
+
     const answerShow = e.currentTarget.childNodes[1].className
     if(answerShow === "answer_invisible"){
       e.currentTarget.childNodes[1].className = "answer_visible"
     }else{
       e.currentTarget.childNodes[1].className = "answer_invisible"
     }
+
   }
+
+  // FIXME: 只點answerShow應該也要能觸發
+  function toggleEffect(e) { 
+    
+    const toggleEffect = e.currentTarget.className;
+    if(toggleEffect === "toggle-closed"){
+      e.currentTarget.className = "toggle-opened"
+    }else{
+      e.currentTarget.className  = "toggle-closed"
+    }
+  }
+
+
 
   return(
 
@@ -21,8 +39,13 @@ function QA () {
         <div className="qa-content"  onClick={(e)=>{answerShow(e)}}>
 
           <div className="question">
-            <img src={qa_plus} alt=""    
-            onClick={()=>{}}></img>
+
+            <div className="toggle-closed" onClick={(e)=>{toggleEffect(e)}}>
+              <div className="circle">
+                <div className="horizontal"></div>
+                <div className="vertical"></div>
+              </div>
+            </div>
 
             <p>回報前，你會需要知道這些～</p>
           </div>
@@ -34,7 +57,14 @@ function QA () {
 
         <div className="qa-content" onClick={(e)=>{answerShow(e)}}>
           <div className="question">
-            <img src={qa_plus} alt="" onClick={()=>{}}></img>
+            
+          <div className="toggle-closed" onClick={(e)=>{toggleEffect(e)}}>
+                <div className="circle">
+                  <div className="horizontal"></div>
+                  <div className="vertical"></div>
+                </div>
+            </div>
+          
             <p>回報時，這些注意事項必看！</p>
           </div>
 
@@ -43,7 +73,12 @@ function QA () {
         
         <div className="qa-content" onClick={(e)=>{answerShow(e)}}>
           <div className="question">
-            <img src={qa_plus} alt="" onClick={()=>{}}></img>
+            <div className="toggle-closed" onClick={(e)=>{toggleEffect(e)}}>
+                <div className="circle">
+                  <div className="horizontal"></div>
+                  <div className="vertical"></div>
+                </div>
+            </div>
             <p>回報後，怎麼了解成效？</p>
           </div>
           
@@ -53,7 +88,12 @@ function QA () {
         
         <div className="qa-content" onClick={(e)=>{answerShow(e)}}>
           <div className="question">
-            <img src={qa_plus} alt="" onClick={()=>{}}></img>
+            <div className="toggle-closed" onClick={(e)=>{toggleEffect(e)}}>
+                <div className="circle">
+                  <div className="horizontal"></div>
+                  <div className="vertical"></div>
+                </div>
+            </div>
             <p>想更加了解這個系統看這邊！</p>
           </div>
           
