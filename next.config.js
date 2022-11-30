7895d03fc4867ae30662a4f3b430fa0c0cf66bda
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2022-11-30 18:41:42 +0800

    chore: styled-component SSR error handle

--- next.config.js
+++ next.config.js
@@ -2,6 +2,49 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: '*',
+      },
+    ],
+  },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
+  webpack: (config, options) => {
+    // graphql files
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'graphql-tag/loader',
+        },
+      ],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  async rewrites() {
+    return [
+      // only for index
+      {
+        source: '/politics-tracker',
+        destination: '/',
+      },
+      // for other page under /politics-tracker
+      {
+        source: '/politics-tracker/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
