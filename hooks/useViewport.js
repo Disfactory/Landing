ff2d212d80e05b08602ee8908aa08a67995c2bdc
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-01-26 16:57:05 +0800

    feat: add viewport hooks

---
+++ hooks/useViewport.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useViewportWidth = () => {
+  const [viewportWidth, setViewportWidth] = useState(0);
+  const [viewportHeight, setViewportHeight] = useState(0);
+
+  const handleRWD = () => {
+    setViewportWidth(window.innerWidth);
+    setViewportHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleRWD);
+    handleRWD();
+
+    return () => {
+      window.removeEventListener('resize', handleRWD);
+    };
+  }, []);
+
+  return {
+    viewportWidth,
+    viewportHeight,
+    isWidtherThanSm: viewportWidth >= 576,
+    isWidtherThanMd: viewportWidth >= 768,
+    isWidtherThanXl: viewportWidth >= 1200,
+  };
+};
+
+export default useViewportWidth;
