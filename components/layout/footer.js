ce4f5ea914a80c3f06f8d00e2a74f5320b887f38
Xuan <104489150+ChangRongXuan@users.noreply.github.com>
2023-02-08 18:17:08 +0800

    feat: add layout components

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,14 +1,19 @@
-import styled from 'styled-components'
-import React from 'react'
-
-const FooterContainer = styled.div``
+import styled from 'styled-components';
+import React, { useState } from 'react';
+import Contributor from '~/components/contributor';
 
 const ContactInfo = styled.div`
   width: 100%;
   padding: 15px 0px;
   text-align: center;
   line-height: 1.8rem;
-`
+
+  a {
+    color: ${({ theme }) => theme.textColor.black};
+    text-decoration: underline;
+  }
+`;
+
 const Credit = styled.div`
   width: 100%;
   padding: 20px 30px;
@@ -25,24 +30,56 @@ const Credit = styled.div`
     margin: auto;
   }
 
-`
-export default function Nav() {
+  span {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
 
+export default function Footer() {
+  const [visible, setVisible] = useState('invisible');
   return (
-   <FooterContainer>
+    <>
       <ContactInfo>
         <p>聯繫地球公民基金會</p>
-        <p>Email: cet@cet-taiwan.org</p>
+        <p>
+          Email：
+          <a
+            href='https://github.com/Disfactory'
+            target='_blank'
+            rel='noreferrer noopenner'
+          >
+            cet@cet-taiwan.org
+          </a>
+        </p>
         <p>電話：02-23920371</p>
-        <p>網站：https://www.cet-taiwan.org</p>
+        <p>
+          網站：
+          <a
+            href='https://www.cet-taiwan.org'
+            target='_blank'
+            rel='noreferrer noopenner'
+          >
+            https://www.cet-taiwan.org
+          </a>
+        </p>
       </ContactInfo>
 
       <Credit>
         <p>
-            全站由使用者回報之資料及設計圖以CC BY姓名標示4.0釋出，標註「農地違章工廠回報系統貢獻者」，
-            原始碼以MIT開放授權公開在GitHub上。使用者回報資料未涉及個人資訊部分已整理成開放資料與API。貢獻者名單
+          全站由使用者回報之資料及設計圖以CC
+          BY姓名標示4.0釋出，標註「農地違章工廠回報系統貢獻者」，
+          原始碼以MIT開放授權公開在GitHub上。使用者回報資料未涉及個人資訊部分已整理成開放資料與API。
+          <span
+            onClick={() => {
+              setVisible('visible');
+            }}
+          >
+            貢獻者名單
+          </span>
         </p>
       </Credit>
-   </FooterContainer>
-  )
+      <Contributor visible={visible} setVisible={setVisible} />
+    </>
+  );
 }
